82e4cfcb7109af026a475ba2f94047890a269287
Asif Ahmed <romeoasif@gmail.com>
2024-09-07 23:32:52 +0600

    project updated

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('admins')->insert(array(
+        	array(
+                'name' => "Asif Ahmed",
+                'email' => 'asifahmed.mist@gmail.com',
+                'password' => bcrypt('test1234'),
+		    )
+		));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\CommonController;
 use App\Http\Middleware\TestMiddleware;
 
+Auth::routes();
+
 Route::namespace('App\Http\Controllers')->group(function () {
 
     Route::get('/', function () {
@@ -27,5 +29,21 @@ Route::namespace('App\Http\Controllers')->group(function () {
 
     Route::get('/about-us', 'AboutUsController@index')->middleware('testMiddleware');
     Route::get('/{pages}', 'CommonController')->name('pages')->where('pages','about|contact|terms');
+
+});
+
+Route::namespace("App\Http\Controllers\Admin")->prefix('admin')->group(function(){
+
+    Route::get('/dashboard', 'DashboardController@index')->name('admin.home');
+    Route::resource('/category', 'CategoryController');
+
+    Route::namespace('Auth')->group(function(){
+        Route::get('/login', 'logincontroller@showloginform')->name('admin.login');
+        Route::post('/login', 'LoginController@login');
+        Route::post('/logout', 'LoginController@loggedout')->name('admin.logout');
+
+    });
+
+
 
 });
